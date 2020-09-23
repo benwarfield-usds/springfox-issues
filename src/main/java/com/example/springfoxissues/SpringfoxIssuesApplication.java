3b888028ecc9dcd4285286564f1f34180293c2e9
Ben Warfield <ben.warfield@usds.dhs.gov>
2020-09-23 13:47:30 -0400

    Initial project skeleton from initializr.

---
+++ src/main/java/com/example/springfoxissues/SpringfoxIssuesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfoxissues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfoxIssuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfoxIssuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springfoxissues/SpringfoxIssuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfoxissues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfoxIssuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
